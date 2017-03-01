71facbf2f0f903a138f3e0ff8612d10da69386ce
Efran Sucipto Lumban Gaol <efransuciptolumbangaol@gmail.com>
2017-03-01 17:20:41 +0700

    Add files via upload

---
+++ Mahasiswa.java
@@ -0,0 +1,8 @@
+// program Mahasiswa
+class Mahasiswa {
+  public static void main(String argv [] ) {
+  System.out.println ("Nama   : Efran Sucipto Lumban Gaol ");
+  System.out.println ("NIM    : 10115418");
+  System.out.println ("Alamat : Bandung,Jawa Barat");
+  }
+ }
